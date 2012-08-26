49c6ae353db897ac15b337e925e17000b4a216ae
Jason Brennan <i.jasonbrennan@gmail.com>
2012-08-26 19:56:09 -0400

    Setting up to do some parsing

--- Scrub/JBScrubbingTextView.m
+++ Scrub/JBScrubbingTextView.m
@@ -8,21 +8,69 @@
 
 #import "JBScrubbingTextView.h"
 
+@interface JBScrubbingTextView () <NSTextStorageDelegate>
+@end
+
+
 @implementation JBScrubbingTextView
 
-- (id)initWithFrame:(NSRect)frame
-{
+- (id)initWithFrame:(NSRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code here.
+		[self commonInitWithFrame:frame];
     }
     
     return self;
 }
 
-- (void)drawRect:(NSRect)dirtyRect
-{
-    // Drawing code here.
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+	self = [super initWithCoder:aDecoder];
+	if (self) {
+		[self commonInitWithFrame:[self frame]];
+	}
+	return self;
 }
+
+
+- (void)commonInitWithFrame:(CGRect)frame {
+	[[self textStorage] setDelegate:self];
+}
+
+
+- (void)textStorageDidProcessEditing:(NSNotification *)notification {
+	[self parseText];
+}
+
+
+- (void)parseText {
+	[self parseMath];
+	[self highlightText];
+}
+
+
+- (void)parseMath {
+	
+}
+
+
+
+- (void)highlightText {
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
