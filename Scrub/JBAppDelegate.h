ee64719e662d608bcd9392d970955b51302a420c
Jason Brennan <i.jasonbrennan@gmail.com>
2012-08-26 18:49:33 -0400

    Initial Commit

---
+++ Scrub/JBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JBAppDelegate.h
+//  Scrub
+//
+//  Created by Jason Brennan on 12-08-26.
+//  Copyright (c) 2012 Jason Brennan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface JBAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
